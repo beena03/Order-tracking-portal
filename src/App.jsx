101cb623a75a1a906369873170bbb2a3816a378b
beena03 <beena@abbyroseinc.com>
2026-06-11 16:42:58 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function OrderPortal() {
     const [password, setPassword] = useState('');
 
     // FIXED: Encoded the raw & symbol to prevent Vercel compilation breaks
-    const logoUrl = "public/Logo svg.png";
+    const logoUrl = "public/Logo.png";
 
     const handleLogin = (e) => {
         e.preventDefault();
